7c6e2243218dd0981073b93879276ec10c6cbee1
USER <jeffery4805013@gmail.com>
2019-03-24 23:49:26 +0800

    create view

--- Workshop1/js/script.js
+++ Workshop1/js/script.js
@@ -1,4 +1,3 @@
-
 var bookDataFromLocalStorage = [];
 var bookCategoryList = [
     { text: "資料庫", value: "database", src: "image/database.jpg" },
@@ -14,6 +13,82 @@ function loadBookData() {
     if (bookDataFromLocalStorage == null) {
         bookDataFromLocalStorage = bookData;
         localStorage.setItem('bookData', JSON.stringify(bookDataFromLocalStorage));
+    } else {
+        //console.log(bookDataFromLocalStorage.length);
+        $("#products").kendoMultiSelect({
+            placeholder: "Select products...",
+            dataTextField: "BookName",
+            dataValueField: "BookAuthor",
+            autoBind: true,
+            dataSource: {
+                data: bookData
+            }
+        });
+        kendo.culture("zh-TW");
+        $("#book_grid").kendoGrid({
+            dataSource: {
+                data: bookData,
+                schema: {
+                    model: {
+                        fields: {
+                            BookId: { type: "number" },
+                            BookName: { type: "string" },
+                            BookCategory: { type: "string" },
+                            BookAuthor: { type: "string" },
+                            BookBoughtDate: { type: "date" },
+                            BookDeliveredDate: { type: "boolean" },
+                            BookPrice: { type: "number" },
+                            BookAmount: { type: "number" },
+                            BookTotal: { type: "number" }
+                        }
+                    }
+                },
+                serverPaging: true,
+                serverFiltering: true,
+                pageSize: 20
+            },
+            height: 550,
+            groupable: false, // title
+            sortable: true, // 排序
+            pageable: {
+                input: true,
+                numeric: false,
+                //refresh: false,
+                //pageSizes: false, // 下方按鈕
+                //buttonCount: 5,
+            },
+            columns: [
+                {
+                    field: "BookId",
+                    title: "書籍編號",
+                }, {
+                    field: "BookName",
+                    title: "書籍名稱"
+                }, {
+                    field: "BookCategory",
+                    title: "書籍種類"
+                }, {
+                    field: "BookAuthor",
+                    title: "作者"
+                }, {
+                    field: "BookBoughtDate",
+                    title: "購買日期",
+                    format: "{0: yyyy-MM-dd}"
+                }, {
+                    field: "BookDeliveredDate",
+                    title: "送達狀態"
+                }, {
+                    field: "BookPrice",
+                    title: "金額"
+                }, {
+                    field: "BookAmount",
+                    title: "數量"
+                }, {
+                    field: "BookTotal",
+                    title: "總計",
+                    format: "{0:n0}元"
+                }]
+        });
     }
 }
 
